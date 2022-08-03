5f9eefcab91f23ccb2ae168f24c81c6e0706cc86
tefinirinasarobidy <fanomezantsoatefinirina@gmail.com>
2022-08-03 19:28:47 +0200

    modif auth

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,17 +9,24 @@ use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $request->validate([
+        $rules = [
             'firstname' => 'required',
             'email' => 'required||email ||unique:customers',
             'lastname' => 'required',
             'username' => 'required || unique:customers'
-        ]);
+        ];
+        $validate = Validator::make($request->all(), $rules);
+        if ($validate->fails()) {
+            return response()->json([
+                'warning' => $validate->errors()
+            ]);
+        }
         $data = [
             'firstname' => $request->firstname,
             'lastname' => $request->lastname,
@@ -55,29 +62,37 @@ class AuthController extends Controller
     {
         $login_type = filter_var($request->email, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
         $user = Customers::where($login_type, $request->email)->first();
-        if(!Hash::check($request->password, $user->password)){
+        if ($user) {
+            if(!Hash::check($request->password, $user->password)){
+                return [
+                   'error' => 'Credentials wrong',
+                   'message' => 'Credentials wrong',
+                   'status_code' => 401
+               ];
+            }
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->token;
+    
+            $token->expires_at = $request->remember_me ? Carbon::now()->addWeeks(1) : Carbon::now()->addDay(1);
+            $token->save();
+            $access_token = $tokenResult->accessToken;
+            $token_type = 'Bearer';
+            $expires_at = Carbon::parse(
+                $tokenResult->token->expires_at
+            )->toDateTimeString();
+            return response()->json([
+                'access_token' => $access_token,
+                'token_type' => $token_type,
+                'expires_at' => $expires_at,
+                'user' => $user->id
+            ], 200);
+        }else {
             return [
-               'error' => 'Credentials wrong',
-               'message' => 'Credentials wrong',
-               'status_code' => 401
-           ];
+                'error' => 'Credentials wrong',
+                'message' => 'Credentials wrong',
+                'status_code' => 401
+            ];
         }
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-
-        $token->expires_at = $request->remember_me ? Carbon::now()->addWeeks(1) : Carbon::now()->addDay(1);
-        $token->save();
-        $access_token = $tokenResult->accessToken;
-        $token_type = 'Bearer';
-        $expires_at = Carbon::parse(
-            $tokenResult->token->expires_at
-        )->toDateTimeString();
-        return response()->json([
-            'access_token' => $access_token,
-            'token_type' => $token_type,
-            'expires_at' => $expires_at,
-            'user' => $user->id
-        ], 200);
     }
      public function logout(Request $request)
      {
